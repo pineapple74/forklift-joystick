f6d5466569153abd9fbb32b00a08e39ddf083eec
pineapple74 <122548032+pineapple74@users.noreply.github.com>
2023-04-26 11:32:11 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -23,3 +23,31 @@ basic.showLeds(`
     # # # # #
     # . # . #
     `)
+basic.forever(function () {
+    if (joystickbit.getRockerValue(joystickbit.rockerType.Y) > 800) {
+        radio.sendNumber(1)
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) < 200) {
+        radio.sendNumber(2)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
+        radio.sendNumber(6)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P14)) {
+        radio.sendNumber(7)
+    }
+})
+basic.forever(function () {
+    if (action == 1) {
+        forward()
+    }
+    if (action == 2) {
+        backward()
+    }
+    if (action == 6) {
+        fork_up()
+    }
+    if (action == 7) {
+        fork_down()
+    }
+    if (action == 0) {
+        wuKong.stopAllMotor()
+    }
+})
